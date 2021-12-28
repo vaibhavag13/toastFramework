17c6ba419ba1bc721f0023cc9380d2a68ec5a804
Vaibhav <v.agarwal@logictrixtech.com>
2021-12-28 11:41:27 +0530

    changed

--- ToastLibrary/src/main/java/com/vaibhav/toastlibrary/MyClass.java
+++ ToastLibrary/src/main/java/com/vaibhav/toastlibrary/MyClass.java
@@ -1,4 +1,10 @@
 package com.vaibhav.toastlibrary;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class MyClass {
+    private static void printToast (String s , Context c ){
+        Toast.makeText(c, s, Toast.LENGTH_SHORT).show();
+    }
 }
